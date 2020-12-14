fd63f56272110cf6bb9d6fabbfb1c887360ae78f
Francisco Beltrami Martinez <francisco.beltrami@alumnos.uv.cl>
2020-12-14 11:25:42 -0300

    Video

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -10,17 +10,21 @@ const Principal = () =>{
         
         <div className="ui fluid container">
             <div className="ui celled grid ">
-                <div class="ui stackable two column grid">
-                    <div class="three wide column">
-                        <div class="ui segment">
+                <div className="ui stackable two column grid">
+                    <div className="three wide column">
+                        <div className="ui segment">
                             <Memenu/>
                         </div>
                     </div>
-                    <div class="thirteen wide column">
+                    <div className="thirteen wide column">
                         <div class="ui segment">
                             <h1>Bienvenido a Belmingo</h1>
                             <p>Dentro de este sitio web podras verificar tu RUT e ingresar tus datos 
                             personales básicos, sin ningún segundo fin, no se preocupe, no le pasara nada.</p>
+                            <p>Recuerde sonreir y romper el sistema</p>
+                            <div class="embed-responsive embed-responsive-16by9">
+                            <iframe width="560" height="315" src="https://www.youtube.com/embed/wO6f3Zk9Kfg" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
                         </div>
                     </div>
                 </div>
@@ -28,25 +32,6 @@ const Principal = () =>{
             <div className="ui right aligned container version-inicio">version: {version}</div>
         </div>
 
-
-
-        // <div className="ui fluid container">
-        //     <br/>
-        //    <div className="ui celled grid ">
-        //         <div className="ui justify aligned middle aligned grid">
-        //             <div className=" column">
-        //                 <Memenu/>
-        //             </div>
-        //             <div className="twelve wide column">
-        //                 <div className="ui segment">
-        //                     <h1>Bienvenido a Belmingo</h1>
-        //                     <p>Dentro de este sitio web podras verificar tu RUT e ingresar tus datos 
-        //                     personales básicos, sin ningún segundo fin, no se preocupe, no le pasara nada.</p>
-        //                 </div>
-        //             </div>
-        //         </div>
-        //     </div> 
-        //     <div className="ui right aligned container version-inicio">version: {version}</div>
         
         
     )
